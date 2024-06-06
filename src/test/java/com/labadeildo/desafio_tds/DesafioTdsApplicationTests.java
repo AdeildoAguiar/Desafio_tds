74ff35dc0c63e10018a2a1d756175f26fff0676b
AdeildoAguiar <93451684+AdeildoAguiar@users.noreply.github.com>
2024-06-06 18:52:47 -0300

    test: adicionar testes para a classe DesafioTdsApplication

---
+++ src/test/java/com/labadeildo/desafio_tds/DesafioTdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labadeildo.desafio_tds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
