adcd41117b1fd95ec5ed253d121549f41be83c93
AdeildoAguiar <93451684+AdeildoAguiar@users.noreply.github.com>
2024-06-06 18:39:51 -0300

    feat: adicionar exceções personalizadas e handler global de exceções

---
+++ src/main/java/com/labadeildo/desafio_tds/exception/InvalidUrlException.java
@@ -0,0 +1,7 @@
+package com.labadeildo.desafio_tds.exception;
+
+public class InvalidUrlException extends RuntimeException {
+    public InvalidUrlException(String message) {
+        super(message);
+    }
+}
